d25dcc21363434af4f367abfe46b1db8b8f4030b
EM <visualprincess1999@hotmail.com>
2026-03-04 13:25:28 +0200

    task list

--- src/app/core/services/task-service/task-service.ts
+++ src/app/core/services/task-service/task-service.ts
@@ -1,64 +1,76 @@
-import { computed, effect, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
 import { ITask, TaskStatus } from '../../../models/interfaces/itask';
 import { environment } from '../../environment/environment';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
+  private readonly ID = inject(PLATFORM_ID);
+
   private tasks = signal<ITask[]>([]);  //writable
   public readonly allTasks = this.tasks.asReadonly();  //readonly
 
   readonly completedTasks = computed(() => this.tasks().filter(task => task.status === 'completed'));  //computed
-  
-  public readonly completionPercentage = computed(()=>{
-    if(this.tasks().length === 0 ){
+
+  public readonly completionPercentage = computed(() => {
+    if (this.tasks().length === 0) {
       return 0;
     }
-    
+
     const completedTasksLength = this.completedTasks().length;
     return (completedTasksLength / this.tasks().length) * 100;
   });
 
 
-  private saveEffectTasks = effect(()=>{
-    localStorage.setItem(environment.localStorageKey, JSON.stringify(this.tasks()));
+  private saveEffectTasks = effect(() => {
+    if (isPlatformBrowser(this.ID)) {
+      localStorage.setItem(environment.localStorageKey, JSON.stringify(this.tasks()));
+
+    }
   });
 
 
   private nextId = 1;
 
   constructor() {
-    const saved = localStorage.getItem(environment.localStorageKey);
-    if (saved) {
-      this.tasks.set(JSON.parse(saved));
-      this.nextId = this.tasks().length > 0 ? Math.max(...this.tasks().map(t => t.id)) + 1 : 1;
+
+    if (isPlatformBrowser(this.ID)) {
+
+
+      const saved = localStorage.getItem(environment.localStorageKey);
+      if (saved) {
+        this.tasks.set(JSON.parse(saved));
+        this.nextId = this.tasks().length > 0 ? Math.max(...this.tasks().map(t => t.id)) + 1 : 1;
+      }
     }
+
   }
 
-  addTask(task: ITask){
-    const newTask:ITask  ={
+  addTask(taskTitle: string) {
+    const newTask: ITask = {
       id: this.nextId++,
-      title: task.title,
+      title: taskTitle,
       status: 'pending'
     };
     this.tasks.update(tasks => [...tasks, newTask]);
 
-    }
-    
-    
-    updateTaskStatus(id:number, status:TaskStatus){
-      this.tasks.update(tasks => tasks.map(task => task.id === id ? {...task, status} : task));
-    }
+  }
 
-    deleteTask(id:number){
-      this.tasks.update(tasks => tasks.filter(task => task.id !== id));
-    }
 
-    sortTasks(){
-      this.tasks.update(
-        tasks=>[...tasks].sort((a,b) => a.id - b.id)
-      );
-    }
+  updateTaskStatus(id: number, status: TaskStatus) {
+    this.tasks.update(tasks => tasks.map(task => task.id === id ? { ...task, status } : task));
+  }
+
+  deleteTask(id: number) {
+    this.tasks.update(tasks => tasks.filter(task => task.id !== id));
+  }
+
+  sortTasks() {
+    this.tasks.update(
+      tasks => [...tasks].sort((a, b) => a.id - b.id)
+    );
+  }
 
 }
--- src/app/pages/taskpad/taskpad.ts
+++ src/app/pages/taskpad/taskpad.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { TaskService } from './../../core/services/task-service/task-service';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ITask } from '../../models/interfaces/itask';
 
 @Component({
   selector: 'app-taskpad',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './taskpad.html',
   styleUrl: './taskpad.scss',
 })
 export class Taskpad {
 
+  taskService: TaskService = inject(TaskService);
+
+  newTaskTitle: string = '';
+  tasksList: ITask[] = this.taskService.allTasks();
+  
+
+  addNewTask() {
+
+    if (!this.newTaskTitle.trim()) return;
+
+
+    this.taskService.addTask(this.newTaskTitle.trim());
+    this.newTaskTitle = '';
+
+  }
+
+  toggleTask(task:ITask){
+
+  }
+
+  deleteTask(task:ITask){
+    this.taskService.deleteTask(task.id);
+  }
 }
